b8cb049cf2080328def83ec71bbaa8c6de4bf628
sahilmokal454 <sahilmokal454@gmail.com>
2026-08-18 01:47:16 +0530

    check 1

--- ai-service/config.py
+++ ai-service/config.py
@@ -4,4 +4,4 @@ import os
 
 ELASTIC_HOST = os.getenv("ELASTIC_HOST", "http://localhost:9200")
 TIME_PROPAGATION_WINDOW_SECONDS = int(os.getenv("TIME_PROPAGATION_WINDOW_SECONDS", 30))
-ERROR_BURST_THRESHOLD = 5              # minimum errors to consider burst
+ERROR_BURST_THRESHOLD = 5              # minimum errors to consider burs
